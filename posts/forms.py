7221be3ca6bfe7361868561b3929c51b1a050ad7
anonimus228-luchiy <computerpc@gmail.com>
2025-03-22 21:10:29 +0600

    hw6

--- posts/forms.py
+++ posts/forms.py
@@ -1,5 +1,8 @@
 from django import forms
-from posts.models import Post
+from django.template.defaulttags import querystring
+
+from posts.models import Post, Category
+
 
 # class PostCreateForm(forms.Form):
 #     image = forms.ImageField()
@@ -42,3 +45,20 @@ def clean(self):
     if title and content and title.lower() in content.lower():
         raise forms.ValidationError("Title should not be in content")
     return cleaned_data
+
+class SearchForm(forms.Form):
+    search = forms.CharField(max_length=100,required=False)
+    category = forms.ModelChoiceField(
+        queryset=Category.objects.all(),required=False,widget=forms.Select
+    )
+    orderings =(
+        ("created_at","По дате создания "),
+        ("-created_at","По дате создания в обратном порядке"),
+        ("title","По названию"),
+        ("-title","По названию в обратном порядке"),
+        ("rate" ,"По рейтингу"),
+        ("-rate","По рейтингу в обратном порядке"),
+        (None ,"Без сортировки")
+
+    )
+    ordering = forms.ChoiceField(choices=orderings,required=False)
--- posts/views.py
+++ posts/views.py
@@ -3,8 +3,10 @@ from django.http import HttpResponse
 import random
 from posts.models import Post
 from django.contrib.auth.decorators import login_required
-from posts.forms import PostCreateForm2
+from posts.forms import PostCreateForm2, SearchForm
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+
 
 def test_view(request):
     return HttpResponse(f"Hello, Wrld! {random.randint(1, 100)}")
@@ -18,8 +20,36 @@ def home_page_view(request):
 @login_required(login_url="login")
 def post_list_view(request):
     posts = Post.objects.all()
+    limit = 4
     if request.method == "GET":
-        return render(request, "posts/post_list.html", context={"posts": posts})
+        search = request.GET.get("search")
+        category = request.GET.get("category")
+        ordering = request.GET.get("ordering")
+        page = int(request.GET.get("page")) if request.GET.get("page") else 1
+        form = SearchForm(request.GET)
+        if search:
+            posts = posts.filter(
+                Q(title__icontains=search) | Q(content__icontains=search)
+            )
+        if category:
+            posts = posts.filter(category=int(category))
+        if ordering:
+            posts = posts.order_by(ordering)
+
+        max_pages = posts.count() / limit
+        if round(max_pages) < max_pages:
+            max_pages = round(max_pages) + 1
+        else:
+            max_pages = round(max_pages)
+
+        start = (page - 1) * limit
+        end = page * limit
+        posts = posts[start:end]
+        context_data = {
+            "posts": posts,
+            "form": form,
+            "max_pages": range(1, max_pages + 1)}
+        return render(request, "posts/post_list.html", context=context_data)
 
 
 @login_required(login_url="login")
@@ -27,10 +57,11 @@ def post_detail_view(request, post_id):
     post = Post.objects.get(id=post_id)
     return render(request, "posts/post_detail.html", context={"post": post})
 
+
 def post_create_view(request):
     if request.method == "GET":
         form = PostCreateForm2()
-        return render(request, "posts/post_create.html" , context={"form": form})
+        return render(request, "posts/post_create.html", context={"form": form})
     if request.method == "POST":
         form = PostCreateForm2(request.POST, request.FILES)
         if not form.is_valid():
@@ -41,7 +72,5 @@ def post_create_view(request):
             # content = form.cleaned_data.get("content")
             # image = form.cleaned_data.get("image")
             # post = Post.objects.create(title=title, content=content,image=image)
-        # if post:
+            # if post:
             return redirect("/posts/")
-
-
